80d814f3b4f981c945bbaebd53744bac4db75549
aman-antino <aman.gupta@antino.com>
2025-06-02 18:22:04 +0530

    feat: add index file to export common components

---
+++ src/components/common/index.ts
@@ -0,0 +1,6 @@
+export * from './button/button';
+export * from './typography/typography';
+export * from './table/table';
+export * from './pagination/pagination';
+export * from './input/input';
+export * from './search-input/search-input';
