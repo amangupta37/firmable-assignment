431e9c3361e53f08a0e8585600ec8ac028988b72
aman-antino <aman.gupta@antino.com>
2025-06-02 18:22:24 +0530

    feat: add typography classes for consistent text styling

---
+++ src/design/index.ts
@@ -0,0 +1,28 @@
+export const typographyClasses = {
+    // Display
+    d1: 'text-[96px] font-medium leading-[110px] tracking-[-2.5px] font-inter text-slate-900',
+
+    // Heading
+    h1: 'text-[64px] font-medium leading-[78px] tracking-[-2.3px] font-inter text-slate-900',
+    h2: 'text-[56px] font-medium leading-[66px] tracking-[-1.75px] font-inter text-slate-900',
+    h3: 'text-[40px] font-medium leading-[48px] tracking-[-1.5px] font-inter text-slate-900',
+    h4: 'text-[32px] font-medium leading-[42px] tracking-[-0.75px] font-inter text-slate-900',
+    h5: 'text-[24px] font-semibold leading-[34px] tracking-[-0.4px] font-inter text-slate-900',
+    h6: 'text-xl font-medium leading-7 text-neutral-900',
+
+    // Body
+    b1: 'text-[20px] font-normal leading-[27px] tracking-[-0.3px] font-inter text-slate-900',
+    b2: 'text-[18px] font-normal leading-[24px] tracking-[-0.4px] font-inter text-slate-900',
+    b3: 'text-[16px] font-normal leading-[24px] tracking-[-0.3px] font-inter text-slate-900',
+    b4: 'text-[14px] font-normal leading-[22px] tracking-[-0.2px] font-inter text-slate-900',
+    b5: 'text-[12px] font-normal leading-[18px] tracking-[-0.2px] font-inter text-slate-900',
+
+    // Label
+    l1: 'text-[24px] font-normal leading-[32px] tracking-[-0.4px] font-inter text-slate-900',
+    l2: 'text-[20px] font-normal leading-[27px] tracking-[-0.4px] font-inter text-slate-900',
+    l3: 'text-[18px] font-normal leading-[24px] tracking-[-0.4px] font-inter text-slate-900',
+    l4: 'text-[16px] font-normal leading-[24px] tracking-[-0.3px] font-inter text-slate-900',
+    l5: 'text-[14px] font-normal leading-[20px] tracking-[-0.2px] font-inter text-slate-900',
+    l6: 'text-[12px] font-medium leading-[16px] tracking-[-0.2px] font-inter text-slate-900',
+    l7: 'text-[10px] font-normal leading-[14px] tracking-[-0.2px] font-inter text-slate-900',
+};
